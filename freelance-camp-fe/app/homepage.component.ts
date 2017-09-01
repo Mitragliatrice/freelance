1b702ee4a0099d09013a5534485bba3568ae3fc6
Colton Mathews <mathews.colton@gmail.com>
2017-09-01 16:16:11 -0600

    base app created

---
+++ freelance-camp-fe/app/homepage.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'main-app',
+  template: '<h1>Freelance Bootcamp Homepage Holy Hannah Montana</h1>'
+})
+export class HomepageComponent { };
