b8ca12683da8bf2c68ff9d8366d62901b6c7de36
Colton Mathews <mathews.colton@gmail.com>
2017-09-04 14:42:44 -0600

    Structured app. Built App component and ran hompage through it.

---
+++ freelance-camp-fe/app/app.component.ts
@@ -0,0 +1,13 @@
+import { selector } from 'rxjs/operator/publish';
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app',
+  template: '<h1>Navigation</h1>'
+
+})
+
+export class AppComponent {
+  title: 'Freelance Bootcamp Dashboard';
+}
