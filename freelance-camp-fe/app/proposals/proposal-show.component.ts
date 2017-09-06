8084bb304b8b27d021e7eb1a15be507c9b26a8be
Colton Mathews <mathews.colton@gmail.com>
2017-09-06 13:51:45 -0600

    added proposal show dynamic route.

---
+++ freelance-camp-fe/app/proposals/proposal-show.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  moduleId: module.id,
+  selector: 'prosposal-show',
+  templateUrl: 'proposal-show.component.html'
+})
+export class ProposalShowComponent {}
